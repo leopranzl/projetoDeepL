74bc35d62f57de7ff52cd744bb58b5f635ff2545
Leo <leopranzl07@gmail.com>
2025-07-30 21:24:51 -0300

    15o teste

--- classify.py
+++ classify.py
@@ -10,6 +10,7 @@ IMG_WIDTH = 224
 IMG_HEIGHT = 224
 EPOCHS = 50
 BATCH_SIZE = 32
+SEED = 123
 
 def load_data(data_dir):
     train_dir = os.path.join(data_dir, "train")
@@ -40,16 +41,13 @@ def load_data(data_dir):
         image_size=(IMG_HEIGHT, IMG_WIDTH),
         batch_size=BATCH_SIZE)
 
-    # Pegue os nomes das classes ANTES do prefetch
     class_names = train_dataset.class_names
 
-    # Agora aplique o prefetch
     AUTOTUNE = tf.data.AUTOTUNE
     train_dataset = train_dataset.prefetch(buffer_size=AUTOTUNE)
     validation_dataset = validation_dataset.prefetch(buffer_size=AUTOTUNE)
     test_dataset = test_dataset.prefetch(buffer_size=AUTOTUNE)
 
-    # Retorne os nomes junto com os datasets
     return train_dataset, validation_dataset, test_dataset, class_names
 
 def show_classes_info(dataset, class_names):
@@ -59,45 +57,60 @@ def show_classes_info(dataset, class_names):
 
     label_counts = pd.Series(labels).value_counts().sort_index()
 
-    # Usa a lista de nomes recebida como parâmetro
     label_counts.index = [class_names[i] for i in label_counts.index]
 
     print("\nImagens por Classe:")
     print(label_counts.sort_index())
 
 def get_model():
-    my_regularizer = tf.keras.regularizers.L2(0.001)
+    # Use apenas a regularização L2 com um valor pequeno
+    # Possivel motivo para  abatch normalization estar atrapalhando o treinamento: dados desbalanceados (classes com muitas e poucas imagens)
+    my_regularizer = tf.keras.regularizers.l2(0.001)
+    
     model = tf.keras.Sequential([
         tf.keras.layers.Rescaling(1./255),
-        tf.keras.layers.RandAugment(value_range=(0, 1), num_ops=2, factor=0.2, interpolation="bilinear", seed=123, data_format=None,),
-        tf.keras.layers.Conv2D(32, (3, 3), input_shape=(IMG_WIDTH, IMG_HEIGHT, 3), padding="same", activation='relu', kernel_regularizer=my_regularizer),
+        tf.keras.layers.RandomFlip("horizontal"),
+        tf.keras.layers.RandomRotation(0.15),
+        tf.keras.layers.RandomZoom(0.15),
+        tf.keras.layers.RandomContrast(0.1),
+        
+        
+        
+        #1o bloco
+        tf.keras.layers.Conv2D(32, (3, 3), input_shape=(IMG_WIDTH, IMG_HEIGHT, 3), padding="same", activation='relu'),
         tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
-        tf.keras.layers.Conv2D(64, (3, 3), padding='same', activation='relu', kernel_regularizer=my_regularizer),
+        
+        #2o bloco
+        tf.keras.layers.Conv2D(64, (3, 3), padding='same', activation='relu'),
         tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
-        tf.keras.layers.Conv2D(128, (3, 3), padding='same', activation='relu', kernel_regularizer=my_regularizer),
+        
+        #3o bloco
+        tf.keras.layers.Conv2D(128, (3, 3), padding='same', activation='relu'),
         tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        
+        #4o bloco
+        tf.keras.layers.Conv2D(256, (3, 3), padding='same', activation='relu'),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        
         tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(256, activation="relu", kernel_regularizer=my_regularizer),
+        tf.keras.layers.Dense(512, activation="relu"),
         tf.keras.layers.Dropout(0.5),
-        tf.keras.layers.Dense(256, activation="relu", kernel_regularizer=my_regularizer),
-        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(256, activation="relu"),
         tf.keras.layers.Dense(NUM_CATEGORIES, activation = "softmax")
     ])
     
     model.compile(
-        optimizer="adam",
+        optimizer='adam',
         loss='sparse_categorical_crossentropy',
         metrics=["accuracy"]
     )
     return model
 
 def main():
-    
     model = get_model()
-    # Capture os class_names retornados pela função
+
     training_set, validation_set, test_set, class_names = load_data(sys.argv[1])
     
-    # Passe os class_names para a função
     print("--- Training Set ---")
     show_classes_info(training_set, class_names)
     print("\n--- Validation Set ---")
@@ -108,7 +121,7 @@ def main():
     callback = tf.keras.callbacks.EarlyStopping(
         monitor='val_accuracy',
         min_delta=0.01,
-        patience=3,
+        patience=5,
         verbose=1,
         mode='auto',
         baseline=None,
@@ -116,11 +129,19 @@ def main():
         start_from_epoch=0
     )
     
+    reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(
+    monitor='val_accuracy',
+    factor=0.5,     # Reduz para metade
+    patience=2,     # Espera 2 épocas sem melhorar
+    min_lr=1e-6,
+    verbose=1
+)
+    
     model.fit(
         training_set,
         validation_data=validation_set,
         epochs=EPOCHS,
-        callbacks=[callback]
+        callbacks=[callback, reduce_lr]
     )
     model.evaluate(test_set)
     
